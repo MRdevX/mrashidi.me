eb14e671674c838b9a9b0242b215a27dbb0b1caa
Mahdi Rashidi <m8rashidi@gmail.com>
2025-08-18 20:14:27 +0200

    chore(sentry): update DSN configuration to use environment variables for Sentry integration

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -1,7 +1,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "***REMOVED***",
+  dsn: process.env.SENTRY_DSN,
 
   tracesSampleRate: 1,
 
--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -1,7 +1,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "***REMOVED***",
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
   integrations: [Sentry.replayIntegration()],
 
