3bccf976b15d339a2f69f333bd09fd3edf63adde
Mahdi Rashidi <m8rashidi@gmail.com>
2026-01-04 12:41:27 +0100

    feat(core): export API_CONFIG from constants for improved configuration access

--- src/lib/core/index.ts
+++ src/lib/core/index.ts
@@ -1,3 +1,4 @@
 export * from "./constants";
+export { API_CONFIG } from "./constants";
 export { type Env, env, getEnv, getRequiredEnv, isDevelopment, isProduction, isTest } from "./env";
 export * from "./logger";
